5e58dae1e3d5cb62bac1b0726361c11eee563c8d
Hana <hkwarner@email.wm.edu>
2018-02-16 16:10:58 -0500

    added pitch conversion

--- tribesat_music/CSV_to_MIDI.py
+++ tribesat_music/CSV_to_MIDI.py
@@ -1,19 +1,17 @@
 import math, struct, sys
 import csv
 from midiutil.MidiFile import MIDIFile
+import sound.py
 
 #assume CSV input--want to convert to format readable by MIDI instruments
 
 def CSV_to_MIDI('CSVfile'):
     with open(CSVfile, 'rb') as data:
         inputData = csv.reader(data)
-
     sensors = 7#number of instruments-->depends on how writes to CSV,
     #but assuming each instrument is in different file, but might need to change
     size = len(inputData)
-
     mf = MIDIFile(1) #can make two channels, but starting with 1
-
     volume = 100
     duration = 1 #can make variable, right now 1 beat
     time = 0 #start time
@@ -27,5 +25,7 @@ def CSV_to_MIDI('CSVfile'):
             track+=1
 
 def data_to_pitch(sensorData):
+    pitch = Sound(sensorData)
+    freq = pitch.from_freq(sensorData)
+    return freq
     #convert data to frequency for note on scale
-    pass
