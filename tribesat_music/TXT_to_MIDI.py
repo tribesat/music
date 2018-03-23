0e4da09cf60aea6f78a47f381951bca30830118f
Hana <hkwarner@email.wm.edu>
2018-03-23 17:02:55 -0400

    Txt to MIDI update 3
    No longer calls external class

--- tribesat_music/TXT_to_MIDI.py
+++ tribesat_music/TXT_to_MIDI.py
@@ -2,9 +2,6 @@ import math, struct, sys
 import csv
 from midiutil.MidiFile import MIDIFile
 from sound import Sound
-#from StringIO import StringIO
-
-#assume CSV input--want to convert to format readable by MIDI instruments
 
 def TXT_to_MIDI(txtfile):
     sensors = 7 #number of instruments, assume organized by columns
@@ -14,33 +11,40 @@ def TXT_to_MIDI(txtfile):
         data.append([float(x) for x in line.split()])
     size=len(data)
     mf = MIDIFile(sensors) #can make two channels, but starting with 1
-    #memFile = StringIO()
     volume = 100
     duration = 1 #can make variable, right now 1 beat
-    #time = 0 #start time
-    channel = 0 #1-16 MIDI channels
+    channel = 0 #1-16 MIDI channels, keeping all on one for now
     tempo = 60 #BPM
     for inst in range(sensors):
         track = inst
-        time = 0
         mf.addTempo(track, time, tempo)
-        
         for i in range(len(data[inst])):
             currentData = data[inst][i]
-            pitch = data_to_pitch(currentData)
-            mf.addNote(track, channel, pitch, time, duration, volume)
-            time += 1
-        #channel +=1
+            pitch = data_to_pitch(currentData, duration, volume)
+            mf.addNote(track, channel, pitch, i, duration, volume) #i==time
 
-#need new method mapping scalar frequencies to MIDI  values
-    with open('output5.mid', 'wb') as output_file:
+
+    with open('output8.mid', 'wb') as output_file:
         mf.writeFile(output_file)
 
 
-def data_to_pitch(sensorData):
-    pitch = Sound(sensorData)
-    freq = pitch.to_MIDI(sensorData)
-    return int(freq)
+def data_to_pitch(sensorData, duration, volume):
+    frequency = sensorData
+    sampleRate = 44100  # of samples per second (standard)
+    if frequency == 0:
+        numSamplesPerCyc = 1
+    else:
+        numSamplesPerCyc = int(sampleRate / frequency)
+    numSamples = int(sampleRate * duration)
+    for i in range(numSamples):
+        sample = 32767 * float(volume) / 100
+        sample *= math.sin(math.pi * 2 *
+                    (i % numSamplesPerCyc) / numSamplesPerCyc)
+    if sample == 0:
+        mv = 0
+    else:
+        mv = 69 + 12 * math.log(abs(sample/440))/math.log(2)
+    return int(abs(mv+4*12)) #bumps up n octaves (midi increments of 12, better sound in library for low freq data)
     #convert data to frequency for note on scale
 
 TXT_to_MIDI('test_data5.txt')
